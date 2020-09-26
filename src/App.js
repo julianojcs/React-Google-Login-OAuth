5a141205f2849d10c0f4988d800a80a9cf631602
Juliano Costa <apfjuliano@gmail.com>
2020-09-25 23:37:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
+ 
+const responseGoogle = (response) => {
+  console.log(response);
+}
+ 
+ReactDOM.render(
+  <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />,
+  document.getElementById('googleButton')
+);
+
 
 function App() {
   return (
